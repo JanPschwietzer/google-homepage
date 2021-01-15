a7d616642083d0cf7b6fb58d08253e6094ac9fa4
Jan Schwietzer <schwietzer.jan@gmail.com>
2021-01-15 05:06:54 +0100

    Added javascript redirect

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-function redirect_google(){
+function redirect_search(){
     let eingabe = document.getElementById("search-request").value;
-    window.location.replace = "http://google.de/search?q=" + eingabe;
+    window.location = "http://google.de/search?q=" + eingabe;
+    return false;
+}
+
+function redirect_first(){
+    let eingabe = document.getElementById("search-request").value;
+    window.location = "http://google.de/search?q=" + eingabe + "&btnI";
     return false;
 }
